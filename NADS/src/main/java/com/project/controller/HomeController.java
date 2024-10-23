b0915da3b419ce5d0a3bc0d5bb99f713418e7ddd
YeonjuJi <bow10004@gmail.com>
2024-10-23 12:48:17 +0900

    spring boot 프로젝트 생성 및 그래프 작업

---
+++ NADS/src/main/java/com/project/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String Home() {
+		return "Home";
+	}
+	
+	@PostMapping("/graphShow")
+	public String graphShow(@RequestParam("gph") String gph) {
+		if(gph.equals("daily")) {
+			return "dayGph";
+		}else if(gph.equals("weekly")) {
+			return "weekGph";
+		}else if(gph.equals("monthly")) {
+			return "monthGph";
+		}else if(gph.equals("monthly")) {
+			return "countryGph"; 
+		}else {
+			return "cpuGph";
+		}
+	}
+	
+	@PostMapping("/elasticTest")
+	public String testPage() {
+		return "elasticTest";
+	}
+	
+}
