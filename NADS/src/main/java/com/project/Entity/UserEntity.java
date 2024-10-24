2c0e594d14d53f4d1eaab46ad82bdb14f713d61f
YeonjuJi <bow10004@gmail.com>
2024-10-24 17:13:05 +0900

    서버 실행 안되는 오류 일단 해결

--- NADS/src/main/java/com/project/Entity/UserEntity.java
+++ NADS/src/main/java/com/project/Entity/UserEntity.java
@@ -1,9 +1,8 @@
 package com.project.Entity;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
