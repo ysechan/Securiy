246794a8522e942c0894edf18df678528639078f
YeonjuJi <bow10004@gmail.com>
2024-10-30 23:40:55 +0900

    데이터 조회 양 최대값 늘리기 대장정 ... ㅠ

--- NADS/src/main/java/com/project/repository/ElasticRepoCustomImpl.java
+++ NADS/src/main/java/com/project/repository/ElasticRepoCustomImpl.java
@@ -32,4 +32,6 @@ public class ElasticRepoCustomImpl implements ElasticRepoCustom {
             IndexCoordinates.of(indexPattern)
         ).map(searchHit -> searchHit.getContent()).toList();
     }
+	
+	
 }
