b33745ef39efa22d58b155704f7060abdb6d364c
YeonjuJi <bow10004@gmail.com>
2024-11-06 15:33:10 +0900

    그래프 페이지가 자꾸 멈춰보림ㅎ

--- NADS/src/main/java/com/project/Entity/ElasticEntity.java
+++ NADS/src/main/java/com/project/Entity/ElasticEntity.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(indexName = "last_log-*")
 public class ElasticEntity {
 
 	@Id
--- NADS/src/main/java/com/project/Entity/ThresholdEntity.java
+++ NADS/src/main/java/com/project/Entity/ThresholdEntity.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Document(indexName = "thrashold")
 public class ThresholdEntity {
 
 	@Id
